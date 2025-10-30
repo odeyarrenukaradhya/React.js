3fd4eb96e11f0c8b1e3e2f083872458d66585f90
Renukaradhya Odeyar C G <odeyarrenukaradhya@gmail.com>
2025-10-30 22:26:11 +0530

    Add files via upload

---
+++ helloWorld.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Hello() {
+  return (
+    <div>
+      <h2><b>HELLO</b></h2>
+      <p><mark>Good Evening everyone</mark></p>
+    </div>
+
+  ) 
+}
+export default Hello
