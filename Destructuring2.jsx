42126f73e4b794e708e946f860c3546c00168eae
Renukaradhya Odeyar C G <odeyarrenukaradhya@gmail.com>
2025-10-31 18:08:26 +0530

    Add files via upload

---
+++ Destructuring2.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Car2({brand,model,color,year}) {
+  return (
+    <h2>I love my {brand} {model} from {year} and it is {color} color!</h2>
+  )
+}
+
+export default Car2;
