ee3af63cf6aa1524f35e43cb5d580f3da79caa78
Renukaradhya Odeyar C G <odeyarrenukaradhya@gmail.com>
2025-11-01 21:31:02 +0530

    Add files via upload

---
+++ HtmlForms.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function HtmlForms() {
+  return (
+    <div>
+      <form>
+        <input type="email" id="user"/>
+        <button>Submit</button>
+      </form>
+    </div>
+  )
+}
+
+export default HtmlForms;
