18113c8b18d470bc356cb9ec3f9a4e2814882c27
Renukaradhya Odeyar C G <odeyarrenukaradhya@gmail.com>
2025-11-02 16:56:42 +0530

    Add files via upload

---
+++ User.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import {useParams} from "react-router-dom";
+
+function User() {
+    const {id}=useParams();
+  return <h2>User Id:{id}</h2>;
+}
+  
+
+export default User
